70dc771a053b3427843fb80baba081c8cc875649
janhavi07 <partejanhavi@gmail.com>
2019-12-05 16:59:12 +0530

    Refactored the code by making Interface class user specific

--- src/main/java/com/addressbook/IAddressBook.java
+++ src/main/java/com/addressbook/IAddressBook.java
@@ -4,5 +4,5 @@ public interface IAddressBook {
     public String createFile(String fileName);
     public String deleteFile(String fileName);
     public AddressDetails add(String firstName, String lastName, String address, String city, String state, String zipcode);
-   public String isFilePresentOrNot(String filePath,String FileName);
+    //public String isFilePresentOrNot(String filePath,String FileName);
 }
--- src/test/java/com/addressbook/AddressTest.java
+++ src/test/java/com/addressbook/AddressTest.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 public class AddressTest {
     private static final String FILE_PATH="/home/admin293/Desktop/AddressBook/AddressBooks/";
-    IAddressBook addressBook=new AddressBook();
+    IAddressBook iaddressBook =new AddressBook();
+    AddressBook addressBook=new AddressBook();
     ArrayList<AddressDetails> personBookList=new ArrayList<>();
     AddressDetails details=new AddressDetails();
 
@@ -23,36 +24,36 @@ public class AddressTest {
 
     @Test
     public void ifFileDoesNotExists_thenReturn_NotPresent() {
-        String isIt=addressBook.isFilePresentOrNot(FILE_PATH,"book.json");
+        String isIt= addressBook.isFilePresentOrNot(FILE_PATH,"book.json");
         Assert.assertEquals("FILE-NOT-PRESENT",isIt);
     }
     @Test
     public void ifFileDoesExists_thenReturn_Present() {
-        String isIt=addressBook.isFilePresentOrNot(FILE_PATH,"book1.json");
+        String isIt= addressBook.isFilePresentOrNot(FILE_PATH,"book1.json");
         Assert.assertEquals("FILE-PRESENT",isIt);
     }
 
     @Test
     public void To_createA_File_ShouldReturn_FileCreated() {
-        String isIt=addressBook.createFile("book2.json");
+        String isIt= iaddressBook.createFile("book2.json");
         Assert.assertEquals("File Created",isIt);
     }
 
     @Test
     public void to_DeleteAFile_ShouldReturn_FileDeleted() {
-        String isIt=addressBook.deleteFile("book2.json");
+        String isIt= iaddressBook.deleteFile("book2.json");
         Assert.assertEquals("File-Deleted",isIt);
     }
 
     @Test
     public void ifIncorrectFileName_to_DeleteAFile_ShouldReturn_FileNotDeleted() {
-        String isIt=addressBook.deleteFile("book3.json");
+        String isIt= iaddressBook.deleteFile("book3.json");
         Assert.assertEquals("Not-Deleted",isIt);
     }
 
     @Test
     public void toCheckDetails_Added_in_PersonDetails() {
-        AddressDetails details= addressBook.add("Janhavi","Parte","FriendsColony",
+        AddressDetails details= iaddressBook.add("Janhavi","Parte","FriendsColony",
                 "Mumbai","Maharashtra","400042");
        Assert.assertEquals("Janhavi",details.getFirstName());
     }
